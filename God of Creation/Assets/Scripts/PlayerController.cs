36780a0b85659827e3ffde67a2c666a3ddd122bb
Sylas Braveheart-Kern <145995602+EnviousOrb@users.noreply.github.com>
2025-02-12 21:05:59 -0500

    Implmented William walk animation

--- God of Creation/Assets/Scripts/PlayerController.cs
+++ God of Creation/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,36 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float speed = 5.0f;
+    public Animator animator;
     private Camera cam;
-    private Vector2 moveDir;
+    private float moveDir;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDir = Input.GetAxisRaw("Horizontal");
+        if(moveDir > 0)
+        {
+            transform.localScale = new Vector3(0.5f, transform.localScale.y, transform.localScale.z);
+        }
+        else if (moveDir < 0)
+        {
+            transform.localScale = new Vector3(-0.5f, transform.localScale.y, transform.localScale.z);
+        }
+        animator.SetFloat("HeroSpeed", Mathf.Abs(moveDir));
     }
 
     void FixedUpdate()
     {
-        transform.Translate(moveDir * speed * Time.fixedDeltaTime);
+        transform.position += new Vector3(moveDir, 0, 0) * speed * Time.deltaTime;
         cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
     }
 }
